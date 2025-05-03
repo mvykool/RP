6819f101d5dc8e0b3275454399c9433fa4ed9f89
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-03 18:20:41 -0400

    redo

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,5 +1,5 @@
-use ferris_says::say
-use std:io::{stdout, BufWriter}
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
 
 fn main(){
     let stdout = stdout();
@@ -7,5 +7,5 @@ fn main(){
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut, writer).unwrap();
+    say(&message, width, &mut writer).unwrap();
 }

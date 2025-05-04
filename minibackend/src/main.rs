1f476e3fcc272876e2aafb685f2943252128a832
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-04 00:30:20 -0400

    created basic get endpoint

--- minibackend/src/main.rs
+++ minibackend/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use actix-web::{get, App, HttpResponse, HttpServer, Responder};
+
+//after importing methods
+//use get macro
+#[get("/pizzas")]
+async fn get_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas are available!")
 }

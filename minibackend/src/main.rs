042848fbcff1f2fa0ca5ba2e8466201c8d321206
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-04 00:39:50 -0400

    added main function

--- minibackend/src/main.rs
+++ minibackend/src/main.rs
@@ -1,8 +1,18 @@
-use actix-web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 //after importing methods
 //use get macro
 #[get("/pizzas")]
 async fn get_pizzas() -> impl Responder {
     HttpResponse::Ok().body("Pizzas are available!")
+}
+
+//main macro, creating entry to the program
+#[actix_web::main]
+//create asyncrounous function named main, with return type result, and use httpserver
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_pizzas))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }

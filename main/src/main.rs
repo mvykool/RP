eb96c930fb7b560f02e776cc58c090e49a784a9f
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-08 10:24:51 -0400

    variable scope understanding

--- main/src/main.rs
+++ main/src/main.rs
@@ -9,3 +9,16 @@ fn main(){
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
 }
+
+//ownership understanding
+
+// RULES TO LIVE BY
+// * each value in Rust has an owner
+// * there can be only one owner
+// * once the owner goes out of scope, the value will be dropped
+
+
+fn app(){               //not valid here, it has not been declared
+    let s = "hello";    //s is valid from this point forward
+    // -- do something  
+}                       //scope ends here, so s is no longer valid

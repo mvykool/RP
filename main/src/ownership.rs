c4bbc9fb11fa5aed1ac78b5cb376713a6cd6a690
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-13 10:02:22 -0400

    copy function

--- main/src/ownership.rs
+++ main/src/ownership.rs
@@ -8,7 +8,7 @@ fn main(){
                                            // the same as if we were moving it to a different
                                            // variable
 
-    println!("{}", s)                     // therefore, this will not run, because the s value
+    println!("{}", s);                     // therefore, this will not run, because the s value
                                           // moved to the function as parameter
 }
 
@@ -16,4 +16,20 @@ fn takes_ownership(string_parameter: String) { // since we are moving s variable
                                                // its now string_parameter variable, once the function
                                                // scope ends, string_parameter is dropped
     println!("{}", string_parameter);
+}
+
+// NOW lets do something simliar but with a copy instead of move 
+
+fn main_copy(){
+    let x: i32 = 5;
+
+    takes_ownership_copy(x);
+
+    println!("{}", x); //unlike the function above, this one will actually print out a copy, since
+                       //takes_ownership_copy copies the variable, it's not moved, just like with
+                       //regular variables, sizable data types are copied
+}
+
+fn takes_ownership_copy(number_parameter: i32){
+    println!("{}", number_parameter);
 }

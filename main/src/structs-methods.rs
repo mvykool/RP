05a441391a52935c85b05d9d08b22416c8f19c0f
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-15 21:35:57 -0400

    creating methods

---
+++ main/src/structs-methods.rs
@@ -0,0 +1,35 @@
+// methods are just like functions, we pass parameters, we return values, they contain some code
+// that's run when the method is called from somewhere else. Unlike functions, methods are defined
+// within the context of a struct, or an enum, or  trait object
+// their first parameter is ALWAYS self, which represents the instace of the struct the method is
+// being called on
+//
+// we use impl 
+// impl is how you attach behavior to types in Rust — it's a core part of the language's approach to object-oriented and trait-based programming.
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// instead of creating the method inside of a object as we would do in JS, here we create an
+// implementation, extending the struct, and adding a method to it, which is just a function inside
+// of a struct lol, always using self, which is the representation of the instance, kind of like
+// `this` in JS
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main(){
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "the area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}

de337819da61a29316a0e54727041d1c1d91c1fe
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-20 09:27:51 -0400

    created struct and impl

--- crates/src/main.rs
+++ crates/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// modules privary
+//
+mod back_of_house {
+
+    pub struct Breakfast {
+        toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches")
+            }
+        }
+    }
 }

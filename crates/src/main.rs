2f9ff0cb64324a965e7eb95a3722ebac1e1b6a2b
Maicol Hernandez <maykolandres4@gmail.com>
2025-05-23 10:17:28 -0400

    use and self

--- crates/src/main.rs
+++ crates/src/main.rs
@@ -26,7 +26,7 @@ pub fn eat_at_restaurant() {
 
 // using enums
 //
-// enums, unlike structs, we dont have to specify if their fields are private or public
+// enums, unlike structs, we dont have to specify if their variants are private or public
 //
 mod back_of_house {
 
@@ -41,3 +41,19 @@ pub fn eat_at_restaurant() {
     let order1: Appetizer = back_of_house::Appetizer::Soup;
     let order2: Appetizer = back_of_house::Appetizer::Salad;
 }
+
+// in rust we can use "use" think of it as import in js, it allow us to bring
+use create::front_of_house::hosting;
+
+//then use like this
+hosting::Soup; //or salad
+
+// we can also use self
+use self::front_of_house::hosting;
+// functions, modules, traits, structs, enums, etc
+// when bringing modules, and functions the idiomatic path should bring the parent function
+// however, when bringing things like enums, traits, structs, its better to put the whole path
+// the only exception is when you are bringing two enums, with the same name, in that case 
+// its better to bring the parent function, we have a context to where it belongs 
+// and clarify is not local
+
